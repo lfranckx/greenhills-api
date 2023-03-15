4de05bbd0ce23ebc076d52c265473fdce8705e09
Bowie <lfranckx@thinkful.com>
2023-03-15 09:52:15 -0500

    install morgan

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 
-const sayHello = (req, res, next) => {
-    res.send("Hello!");
+const sayHello = (req, res) => {
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : "Hello!";
+    res.send(content);
 };
 
-const logging = (req, res, next) => {
-    console.log('A request is being made...');
-    next();
-}
+const saySomething = (req, res) => {
+    const greeting = req.params.greeting;
+    const name = req.query.name;
+  
+    const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+    res.send(content);
+};
 
-app.use(logging);
-app.use(sayHello);
+
+app.use(morgan("dev"));
+app.get("/hello", sayHello);
+app.get("/say/:greeting", saySomething);
 
 module.exports = app;
