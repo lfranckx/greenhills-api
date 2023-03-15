bba11fc853b85eb0acd2b0f0fc607ecbabda0c2a
Bowie <lfranckx@thinkful.com>
2023-03-14 19:17:32 -0500

    add nodemon

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
 
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+};
+
+const logging = (req, res, next) => {
+    console.log('A request is being made...');
+    next();
+}
+
+app.use(logging);
+app.use(sayHello);
+
 module.exports = app;
