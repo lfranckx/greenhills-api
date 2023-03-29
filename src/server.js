0227b1b76ae25a199d55f5aa2619dd8cfacc2332
Bowie <lfranckx@thinkful.com>
2023-03-29 14:41:08 -0500

    update server database connection / add test db connection lines

--- src/server.js
+++ src/server.js
@@ -2,14 +2,24 @@ const app = require('./app');
 const knex = require('knex');
 const { PORT, DATABASE_URL, DB_HOST, DB_USER, DB_PW, DB_NAME } = require('./config');
 
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//       host : DB_HOST,
+//       user : DB_USER,
+//       password : DB_PW,
+//       database : DB_NAME
+//     }
+// });
+
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : DB_HOST,
-      user : DB_USER,
-      password : DB_PW,
-      database : DB_NAME
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
+  }
 });
 
 // Test database connection
@@ -20,6 +30,6 @@ db.select(1)
 app.set('db', db);
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-    console.log(`DB_URL: ${DATABASE_URL}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`DB_URL: ${DATABASE_URL}`);
 });
