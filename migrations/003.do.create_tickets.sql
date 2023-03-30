2406344a44a9d3f62d1d75487916a5a7466f4a47
Bowie <lfranckx@thinkful.com>
2023-03-29 22:00:22 -0500

    modify migration files to represent tickets table correctly

--- migrations/003.do.create_tickets.sql
+++ migrations/003.do.create_tickets.sql
@@ -4,6 +4,7 @@ CREATE TABLE tickets (
     employee_name TEXT NOT NULL,
     employee_id INTEGER REFERENCES employees(id) ON DELETE SET NULL,
     location_id INTEGER NOT NULL REFERENCES locations(id),
-    date_created TYPE TIMESTAMP WITH TIME ZONE USING date_created AT TIME ZONE 'UTC',
+    -- date_created TYPE TIMESTAMP WITH TIME ZONE USING date_created AT TIME ZONE 'UTC',
+    date_created TYPE TIMESTAMP WITHOUT TIME ZONE,
     date_modified TIMESTAMP
 );
