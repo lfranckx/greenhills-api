2248c98c33e57d3b1257095fafe2d0d91ddf5ef4
Bowie <lfranckx@thinkful.com>
2023-03-29 20:36:16 -0500

    modify tickets table to include CST as timezone for date_created

--- migrations/003.do.create_tickets.sql
+++ migrations/003.do.create_tickets.sql
@@ -4,6 +4,6 @@ CREATE TABLE tickets (
     employee_name TEXT NOT NULL,
     employee_id INTEGER REFERENCES employees(id) ON DELETE SET NULL,
     location_id INTEGER NOT NULL REFERENCES locations(id),
-    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
+    date_created TYPE TIMESTAMP WITH TIME ZONE USING date_created AT TIME ZONE 'America/Chicago',
     date_modified TIMESTAMP
 );
